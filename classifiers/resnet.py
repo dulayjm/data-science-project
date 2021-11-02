7cb272d19bc82651266d3d9f141f676f665d4ec7
dulayjm <dulay.justin19@gmail.com>
2021-11-02 11:42:05 -0400

    added basic res

--- classifiers/resnet.py
+++ classifiers/resnet.py
@@ -13,7 +13,7 @@ from torch.utils.data import Dataset
 import os
 import pandas as pd
 import random
-from skimage impoart io
+# from skimage impoart io
 
 from PIL import Image, ImageFilter
 
@@ -71,6 +71,8 @@ class OmniglotReactionTimeDataset(Dataset):
 
 if __name__ == '__main__':
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     transform = transforms.Compose([
         # you can add other transformations in this list
         transforms.ToTensor()
@@ -106,44 +108,122 @@ if __name__ == '__main__':
     validation_loader = torch.utils.data.DataLoader(dataset, batch_size=16,
                                                     sampler=valid_sampler)
 
-    dataiter = iter(train_loader)
-    sample = dataiter.next()
-    X = sample['image1']
-    y = sample['label1']
-
-    X = X.permute(1,2,3,0)
-
-    X = X.numpy()
-    y = y.numpy()
-
-    print('x shape', X.shape)
-    print('y.shape', y.shape)
-
-    dataiter = iter(validation_loader)
-    sample_test = dataiter.next()
-    X_test = sample_test['image1']
-    y_test = sample_test['label1']
+    #### Model work
+    model = torchvision.models.resnet50(pretrained=True)
 
 
-    X_test = X_test.permute(1,2,3,0)
+    # loss function and optimizer
+    criterion = torch.abs_nn.CrossEntropyLoss()
+    optim = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    X_test = X_test.numpy()
-    y_test = y_test.numpy()
 
-    X = X.reshape(X.shape[0]*X.shape[1]*X.shape[2],X.shape[3]).T
-    y = y.reshape(y.shape[0],)
 
-    X_test = X_test.reshape(X_test.shape[0]*X_test.shape[1]*X_test.shape[2],X_test.shape[3]).T
-    y_test = y_test.reshape(y_test.shape[0],)
+    for epoch in range(2):
+        running_loss = 0.0
+        correct = 0.0
+        total = 0.0
 
-    clf = \
-    RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
-           max_depth=None, max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
-           min_samples_split=2, min_weight_fraction_leaf=0.0,
-           n_estimators=10, n_jobs=1, oob_score=False, random_state=None,
-           verbose=0, warm_start=False)
+        for idx, sample in enumerate(train_loader):
+            image1 = sample['image1']
+            image2 = sample['image2']
 
-    clf.fit(X, y)
+            label1 = sample['label1']
+            label2 = sample['label2']
+
+            # if args.loss_fn == 'psych-acc':
+            #     psych = sample['acc']
+            # else: 
+            #     psych = sample['rt']
+
+            # concatenate the batched images for now
+            inputs = torch.cat([image1, image2], dim=0).to(device)
+            labels = torch.cat([label1, label2], dim=0).to(device)
+
+            # psych_tensor = torch.zeros(len(labels))
+            # j = 0 
+            # for i in range(len(psych_tensor)):
+            #     if i % 2 == 0: 
+            #         psych_tensor[i] = psych[j]
+            #         j += 1
+            #     else: 
+            #         psych_tensor[i] = psych_tensor[i-1]
+            # psych_tensor = psych_tensor.to(device)
+
+            outputs = model(inputs).to(device)
+            loss = criterion(outputs, labels)
+
+            # if args.loss_fn == 'cross-entropy':
+            #     loss = loss_fn(outputs, labels)
+            # elif args.loss_fn == 'psych-acc': 
+            #     loss = AccPsychCrossEntropyLoss(outputs, labels, psych_tensor).to(device)
+            # else:
+            #     loss = PsychCrossEntropyLoss(outputs, labels, psych_tensor).to(device)
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            running_loss += loss.item()
+
+            # labels_hat = torch.argmax(outputs, dim=1)  
+            # correct += torch.sum(labels.data == labels_hat)
+
+            # this seemed to fix the accuracy calculation
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+
+    print('Finished Training')
+
+
+
+
+
+
+
+
+
+
+
+    # dataiter = iter(train_loader)
+    # sample = dataiter.next()
+    # X = sample['image1']
+    # y = sample['label1']
+
+    # X = X.permute(1,2,3,0)
+
+    # X = X.numpy()
+    # y = y.numpy()
+
+    # print('x shape', X.shape)
+    # print('y.shape', y.shape)
+
+    # dataiter = iter(validation_loader)
+    # sample_test = dataiter.next()
+    # X_test = sample_test['image1']
+    # y_test = sample_test['label1']
+
+
+    # X_test = X_test.permute(1,2,3,0)
+
+    # X_test = X_test.numpy()
+    # y_test = y_test.numpy()
+
+    # X = X.reshape(X.shape[0]*X.shape[1]*X.shape[2],X.shape[3]).T
+    # y = y.reshape(y.shape[0],)
+
+    # X_test = X_test.reshape(X_test.shape[0]*X_test.shape[1]*X_test.shape[2],X_test.shape[3]).T
+    # y_test = y_test.reshape(y_test.shape[0],)
+
+    # clf = \
+    # RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+    #        max_depth=None, max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+    #        min_samples_split=2, min_weight_fraction_leaf=0.0,
+    #        n_estimators=10, n_jobs=1, oob_score=False, random_state=None,
+    #        verbose=0, warm_start=False)
+
+    # clf.fit(X, y)
 
 
     from sklearn.metrics import accuracy_score
